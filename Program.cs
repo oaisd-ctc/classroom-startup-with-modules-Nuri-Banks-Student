962498996a3e96208ed0dbc167db5ddd5d74dbe1
Nuri-Banks-Student <nubad1b@student.oaisd.org>
2025-10-22 13:59:43 +0000

    content (box)

--- Program.cs
+++ Program.cs
@@ -4,17 +4,45 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Walk in the room.");
+       
+       
+
+        // STEP 3.b - Uncomment the lines below after your functions have been defined
+         FindSeat();
+         SignIn();
+        DailyLogic();
+        BoxBreathing();
+    }
+
+    public static void FindSeat()
+    { Console.WriteLine("Walk in the room.");
         Console.WriteLine("Walk towards desk.");
         Console.WriteLine("Pull out chair.");
         Console.WriteLine("Take off backpack.");
         Console.WriteLine("Place backpack on the ground.");
         Console.WriteLine("Sit down in chair.");
-        Console.WriteLine("Turn on computer.");
+
+    }
+
+    public static void SignIn()
+    { Console.WriteLine("Turn on computer.");
         Console.WriteLine("Sign in to computer.");
         Console.WriteLine("Open browser.");
+      
+    }
+
+    public static void DailyLogic()
+    {
         Console.WriteLine("Navigate to the Daily Logic Classroom.");
         Console.WriteLine("Complete the Daily Logic for the day.");
+
+
+    }
+
+
+    public static void BoxBreathing()
+    {
+
         Console.WriteLine("Inhale for 4 seconds.");
         Console.WriteLine("Hold breath for 4 seconds.");
         Console.WriteLine("Exhale for 4 seconds.");
@@ -42,29 +70,17 @@ public class Program
         Console.WriteLine("Talk to neighbors about Daily Hypothetical.");
         Console.WriteLine("Respond to Daily Hypothetical email.");
 
-        // STEP 3.b - Uncomment the lines below after your functions have been defined
-        // FindSeat();
-        // SignIn();
-        // DailyLogic();
-    }
 
-    public static void FindSeat()
-    {
+
 
     }
 
-    public static void SignIn()
-    {
 
-    }
-
-    public static void DailyLogic()
-    {
-
-    }
 
     /**
     * Create functions for Box Breathing and Daily Hypotheticals below:
     */
+    
+    
 
 }
